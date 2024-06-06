b0b41dea4da929aa4b4c87ceda906749e1b67063
GD124 <gerald.duncan@jpschools.org>
2024-06-06 16:02:39 +0000

    first game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -44,16 +44,23 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
+createPlatform(100, 500, 3000, 10)
 
 
-    
+createPlatform(1,600,1000,10)
+createPlatform(10,300,100,10)
+createPlatform(10,400,100,10)
+createPlatform(600,300,300,10)
+createPlatform(400,300,200,10)
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
-
+createCollectable('database',300, 200, 9.8, 0,10);
+createCollectable('database',300, 100, 9.8, 0,10);
+createCollectable('database',600,100,9.8,0.10)
 
 
     // TODO 3
@@ -62,7 +69,9 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-
+createCannon('bottom',400,1200,100)
+createCannon('bottom', 800,2000,100)
+createCannon('left',300,1000,100)
 
 
     /////////////////////////////////////////////////
